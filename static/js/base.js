b832ddd3599bac27125115ee6d21f6cd398eb3e4
Folarin Ogungbemi <Folarine1@gmail.com>
2023-01-30 16:30:15 +0000

    Add static file

---
+++ static/js/base.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    let headerTag = document.getElementsByTagName('h1')[0];
+    headerTag.innerHTML = "here you are!";
+    console.log(headerTag);
+});
