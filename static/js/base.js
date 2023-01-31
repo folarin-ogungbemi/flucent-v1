a48698d9f286f4ff92d1df44c8e7d0dedbe05684
Folarin Ogungbemi <Folarine1@gmail.com>
2023-01-31 22:22:40 +0000

    Add hamburger button on small screen

--- static/js/base.js
+++ static/js/base.js
@@ -2,9 +2,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     let topNav = document.getElementsByTagName('nav')[0] ;
     topNav.children[1].appendChild(navlistEl);
+    topNav.children[1].appendChild(hamburgerBtn);
 });
-
-const url = window.location.href;
 
 // create dictionary to be iterated on
 const navDict = {
@@ -14,9 +13,9 @@ const navDict = {
     Resume: "#",
 }
 
-// create target element
+// create nav list element
 let navlistEl = document.createElement('ul');
-navlistEl.className = 'list-unstyled d-flex justify-content-around';
+navlistEl.className = 'd-none d-md-flex list-unstyled justify-content-around';
 let navItems = Object.entries(navDict);
 for (item of navItems) {
     let navList = `
@@ -27,3 +26,14 @@ for (item of navItems) {
     </li>`
     navlistEl.innerHTML += navList;
 };
+
+// create hamburger btn element
+const hamburgerBtn = document.createElement('button');
+const hamburgerIcon = document.createElement('div');
+hamburgerBtn.appendChild(hamburgerIcon);
+hamburgerIcon.className = 'hamburger-icon';
+hamburgerBtn.className = 'hamburger-btn d-block d-md-none ms-auto';
+
+hamburgerBtn.addEventListener('click', () => {
+    hamburgerIcon.classList.toggle('is-active');
+})
