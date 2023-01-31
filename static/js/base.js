b0a4923bd669fa9c0ad9d1123f838ec48d5d07b2
Folarin Ogungbemi <Folarine1@gmail.com>
2023-01-31 08:40:17 +0000

    Create top nav elements

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,29 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    let headerTag = document.getElementsByTagName('h1')[0];
-    headerTag.innerHTML = "here you are!";
-    console.log(headerTag);
+// Allow DOM to load first
+document.addEventListener('DOMContentLoaded', () => {
+    let topNav = document.getElementsByTagName('nav')[0] ;
+    topNav.children[1].appendChild(navlistEl);
 });
+
+const url = window.location.href;
+
+// create dictionary to be iterated on
+const navDict = {
+    About: "#",
+    Projects: "#",
+    Contact: "#",
+    Resume: "#",
+}
+
+// create target element
+let navlistEl = document.createElement('ul');
+navlistEl.className = 'list-unstyled d-flex justify-content-around';
+let navItems = Object.entries(navDict);
+for (item of navItems) {
+    let navList = `
+    <li class="nav-item">
+        <a href="${item[1]}"
+        class="nav-link"
+        >${item[0]}</a>
+    </li>`
+    navlistEl.innerHTML += navList;
+};
