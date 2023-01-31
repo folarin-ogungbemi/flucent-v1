49581856de8d67e02140c973cdb7ee5e3419df98
Folarin Ogungbemi <Folarine1@gmail.com>
2023-01-31 23:10:52 +0000

    Add mobile nav list element

--- static/js/base.js
+++ static/js/base.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let topNav = document.getElementsByTagName('nav')[0] ;
     topNav.children[1].appendChild(navlistEl);
     topNav.children[1].appendChild(hamburgerBtn);
+    topNav.children[1].appendChild(mobileNavlistEl);
 });
 
 // create dictionary to be iterated on
@@ -13,9 +14,21 @@ const navDict = {
     Resume: "#",
 }
 
+// create hamburger btn element
+const hamburgerBtn = document.createElement('button');
+const hamburgerIcon = document.createElement('div');
+hamburgerBtn.appendChild(hamburgerIcon);
+hamburgerIcon.className = 'hamburger-icon';
+hamburgerBtn.className = 'hamburger-btn d-block d-md-none ms-auto';
+
 // create nav list element
 let navlistEl = document.createElement('ul');
 navlistEl.className = 'd-none d-md-flex list-unstyled justify-content-around';
+
+// create mobile-nav list element
+let mobileNavlistEl = document.createElement('ul');
+mobileNavlistEl.className = 'd-block d-md-none list-unstyled mobile-nav';
+
 let navItems = Object.entries(navDict);
 for (item of navItems) {
     let navList = `
@@ -25,15 +38,10 @@ for (item of navItems) {
         >${item[0]}</a>
     </li>`
     navlistEl.innerHTML += navList;
+    mobileNavlistEl.innerHTML += navList;
 };
-
-// create hamburger btn element
-const hamburgerBtn = document.createElement('button');
-const hamburgerIcon = document.createElement('div');
-hamburgerBtn.appendChild(hamburgerIcon);
-hamburgerIcon.className = 'hamburger-icon';
-hamburgerBtn.className = 'hamburger-btn d-block d-md-none ms-auto';
 
 hamburgerBtn.addEventListener('click', () => {
     hamburgerIcon.classList.toggle('is-active');
+    mobileNavlistEl.classList.toggle('is-active');
 })
