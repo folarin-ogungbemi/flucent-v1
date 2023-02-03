dd2f890685bd08530204bc0e451d656a80b95031
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-03 20:47:57 +0000

    Add project display method

--- home/static/js/projects.js
+++ home/static/js/projects.js
@@ -38,7 +38,7 @@ let projectList = document.getElementsByClassName('projects')[0];
 for (let name of projectName){
     let project = `
     <ul class="list-unstyled project">
-       <li class="project-btn"><a href="/#${name}">${name}</a></li>
+       <li class="project-btn">${name}</li>
     </ul>`
     projectList.innerHTML += project;
 }
@@ -46,44 +46,61 @@ for (let name of projectName){
 let projectDisplay = document.getElementsByClassName('project-display')[0];
 const projectInfo = projects.map(project => {
     let projectView = 
-    `<div class="project" id="${project.name}">
-        <div class="project-caption h-100 w-100">
-            <div class="project-caption-content">
-                <div class="row">
-                    <div class="col-12 d-flex justify-content-between">
-                        <a href="${project.github}"
-                        target="_blank"
-                        rel="noopener"
-                        aria-label="visit folarin's github page(open in a new tab)"
-                        ><i class="fa-brands fa-github-alt"></i></a>
-                        <a href="${project.url}"
-                        target="_blank"
-                        rel="noopener"
-                        aria-label="visit project's page(open in a new tab)"
-                        ><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+    `<div class="col-8 project-item mx-auto mt-3">
+        <div class="project" id="${project.name}">
+            <div class="project-caption h-100 w-100">
+                <div class="project-caption-content">
+                    <div class="row">
+                        <div class="col-12 d-flex justify-content-between">
+                            <a href="${project.github}"
+                            target="_blank"
+                            rel="noopener"
+                            aria-label="visit folarin's github page(open in a new tab)"
+                            ><i class="fa-brands fa-github-alt"></i></a>
+                            <a href="${project.url}"
+                            target="_blank"
+                            rel="noopener"
+                            aria-label="visit project's page(open in a new tab)"
+                            ><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                        </div>
                     </div>
+                    <div class="row">
+                        <div class="col-12 mt-2">
+                            <h4 class="capitalize project-name">${project.name}</h4>
+                            <p class="small">${project.description}</p>`;
+                                let projectTechList = 
+                                `<ul class="d-flex justify-content-start list-unstyled">`
+                                for (let item of project.technology){
+                                    let language = 
+                                    `<li class="language me-2">${item}</li>`;
+                                    projectTechList += language;
+                                }
+                                projectTechList += `</ul>`
+                                projectView += projectTechList;
+                            projectView += `
+                        </div>
+                    </div> 
                 </div>
-                <div class="row">
-                    <div class="col-12 mt-2">
-                        <h4 class="capitalize project-name">${project.name}</h4>
-                        <p class="small">${project.description}</p>`;
-                            let projectTechList = 
-                            `<ul class="d-flex justify-content-start list-unstyled">`
-                            for (let item of project.technology){
-                                let language = 
-                                `<li class="language me-2">${item}</li>`;
-                                projectTechList += language;
-                            }
-                            projectTechList += `</ul>`
-                            projectView += projectTechList;
-                        projectView += `
-                    </div>
-                </div> 
             </div>
+            <img class="img-fluid" src="${project.image}" alt="project mockup" />
         </div>
-        <img class="img-fluid" src="${project.image}" alt="project mockup" />
     </div>`;
 
     projectDisplay.innerHTML += projectView;
 });                        
-    
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const projectBtn = document.querySelectorAll('.project-btn');
+    const projectContent = document.querySelectorAll('.project-item');
+    projectBtn.forEach((project, item) => {
+        project.addEventListener('click', () =>{
+            // toggle project button
+            projectBtn.forEach(project => project.classList.remove('active'));
+            project.classList.add('active');
+            // toggle project content to display
+            projectContent.forEach(content => content.classList.remove('active'));
+            projectContent[item].classList.add('active');
+        })
+    });
+})
