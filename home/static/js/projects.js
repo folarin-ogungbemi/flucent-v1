6ac533632e6e63c68bcbb9b1999e35aee48d3123
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-03 17:54:13 +0000

    Display project

--- home/static/js/projects.js
+++ home/static/js/projects.js
@@ -38,7 +38,52 @@ let projectList = document.getElementsByClassName('projects')[0];
 for (let name of projectName){
     let project = `
     <ul class="list-unstyled project">
-       <li class="project-btn"><a href="#">${name}</a></li>
+       <li class="project-btn"><a href="/#${name}">${name}</a></li>
     </ul>`
     projectList.innerHTML += project;
 }
+
+let projectDisplay = document.getElementsByClassName('project-display')[0];
+const projectInfo = projects.map(project => {
+    let projectView = 
+    `<div class="project" id="${project.name}">
+        <div class="project-caption h-100 w-100">
+            <div class="project-caption-content">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-between">
+                        <a href="${project.github}"
+                        target="_blank"
+                        rel="noopener"
+                        aria-label="visit folarin's github page(open in a new tab)"
+                        ><i class="fa-brands fa-github-alt"></i></a>
+                        <a href="${project.url}"
+                        target="_blank"
+                        rel="noopener"
+                        aria-label="visit project's page(open in a new tab)"
+                        ><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mt-2 project-info">
+                        <span class="capitalize project-name fs-5">${project.name}</span>
+                        <p class="small">${project.description}</p>`;
+                            let projectTechList = 
+                            `<ul class="d-flex justify-content-start list-unstyled">`
+                            for (let item of project.technology){
+                                let language = 
+                                `<li class="language me-2">${item}</li>`;
+                                projectTechList += language;
+                            }
+                            projectTechList += `</ul>`
+                            projectView += projectTechList;
+                        projectView += `
+                    </div>
+                </div> 
+            </div>
+        </div>
+    <img class="img-fluid" src="${project.image}" alt="project mockup" />
+</div>`
+        
+    projectDisplay.innerHTML += projectView;
+});                        
+    
