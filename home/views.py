d1e45724c4e5973cb55561c34d11e45b431f072c
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-05 19:24:25 +0000

    Submit contact form

--- home/views.py
+++ home/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from home.forms import ContactForm
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
     contact_form = ContactForm()
+
+    if request.method == "POST":
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(
+                    request,
+                    messages.SUCCESS,
+                    f'Thanks, I have receveid your message.')
+            return redirect('home')
+
     context = {'form': contact_form}
     return render(request, 'home/index.html', context)
