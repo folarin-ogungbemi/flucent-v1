8cc26429dad1dcdd088a760d004af1a36cde920f
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-12 12:01:20 +0000

    fix allowed host

--- flucent_developer/settings.py
+++ flucent_developer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['folarin-ogungbemi.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['folarin-ogungbemi.herokuapp.com']
 
 
 # Application definition
