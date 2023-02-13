b2b1d3656a1d884f2e1dd6991749ff45be2f5a17
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-13 18:16:03 +0000

    remove cache control on testing

--- flucent_developer/settings.py
+++ flucent_developer/settings.py
@@ -161,10 +161,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # if 'USE_AWS' in os.environ:
 # cache control
-AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thus, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000'
-    }
+
 
 AWS_STORAGE_BUCKET_NAME = 'folarin-ogungbemi'
 AWS_S3_REGION_NAME = 'us-east-1'
