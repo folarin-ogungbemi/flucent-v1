19a06235c80b27f12da12f650470df79d680218f
Folarin Ogungbemi <Folarine1@gmail.com>
2023-03-31 16:46:06 +0000

    Create views and URLs

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import (
+    TemplateView, CreateView, FormView, ListView, DeleteView)
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from blog.forms import PostForm
+from blog.models import Post
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class BlogView(TemplateView):
+class BlogView(ListView):
+    model = Post
     template_name = 'blog/blog.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.filter(published=True)
+    paginate_by = 5
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/Admin/add_post.html'
+    success_url = reverse_lazy('blog')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/Admin/update_post.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'slug': self.object.slug})
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/Admin/delete_post.html'
+    success_url = reverse_lazy('blog')
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
