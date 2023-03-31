1026a95fc02dde721fbea40e30b4e672655a0df9
Folarin Ogungbemi <Folarine1@gmail.com>
2023-03-31 16:50:17 +0000

    Include static file

---
+++ static/js/blog_base.js
@@ -0,0 +1,9 @@
+window.onload= function() {
+    Particles.init({
+      selector: '.background',
+      color: '#bac0d5',
+      maxParticles: 90,
+      minDistance: 150,
+      connectParticles: false
+    });
+  };
