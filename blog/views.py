a3c2a881b29d593b06b3a1818e27e33d4886188e
Folarin Ogungbemi <Folarine1@gmail.com>
2023-04-04 00:24:39 +0000

    Add comments view and render in template

--- blog/views.py
+++ blog/views.py
@@ -66,26 +66,28 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('blog')
 
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        post = self.get_object()
+        comments = post.comments.filter(approved=True).distinct()
+        context['comments'] = comments
         context['comment_form'] = CommentForm()
         return context
 
-    def form_valid(self, form):
-        if 'comment-form' in self.request.POST:
-            form.instance.author = self.request.user
-            return super().form_valid(form)
-
-    def form_invalid(self, form):
-        return self.render_to_response(
-            self.get_context_data(comment_form=CommentForm()))
-
     def post(self, request, *args, **kwargs):
-        if 'comment-form' in request.POST:
-            self.form_class = CommentForm
-        return super(PostDetailView, self).post(request, *args, **kwargs)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = self.get_object()
+            comment.save()
+            return self.get(request, *args, **kwargs)
+        else:
+            context = self.get_context_data()
+            context['comment_form'] = form
+            return self.render_to_response(context)
