9bc0084112fd9fd9d63310121ffe1f0360bc52ef
Folarin Ogungbemi <Folarine1@gmail.com>
2023-04-04 13:54:46 +0000

    Add privacy policy

--- flucent_developer/urls.py
+++ flucent_developer/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
